eeb48aadb54ed8bb45bea8c3a088dd3d4936260f
PLarboulette <larboulette.pierre@gmail.com>
2016-03-14 16:12:01 +0100

    My bad, forgot file :)

---
+++ src/main/scala/perso/iadvize/utils/MalformedURLExceptionMapper.scala
@@ -0,0 +1,22 @@
+package perso.iadvize.utils
+
+import java.net.MalformedURLException
+
+import com.google.inject.Inject
+import com.twitter.finagle.httpx.{Request, Response}
+import com.twitter.finatra.http.exceptions.ExceptionMapper
+import com.twitter.finatra.http.response.ResponseBuilder
+
+/**
+  * Created by pierre on 14/03/16.
+  */
+
+
+class MalformedURLExceptionMapper @Inject()(response: ResponseBuilder)
+  extends ExceptionMapper[MalformedURLException] {
+
+  override def toResponse(request: Request, exception: MalformedURLException): Response = {
+    response.badRequest(s"Malformed URL - ${exception.getMessage}")
+  }
+}
+
